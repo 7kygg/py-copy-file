bc9d15a1b396195db685668facb733c966bb94a9
Yaroslav Matviienko <volodimirovicaroslav7@gmail.com>
2025-08-04 22:42:20 +0200

    Solution

--- app/main.py
+++ app/main.py
@@ -2,14 +2,17 @@ def copy_file(command: str) -> None:
     command_parts = command.strip().split()
 
     if len(command_parts) != 3:
+        print("The command format is incorrect")
         return
 
     command_name, source_file_name, destination_file_name = command_parts
 
     if command_name != "cp":
+        print("The command is not 'cp'")
         return
 
     if source_file_name == destination_file_name:
+        print("The source and destination file names are the same")
         return
 
     try:
@@ -18,4 +21,6 @@ def copy_file(command: str) -> None:
               ):
             destination_file.write(source_file.read())
     except FileNotFoundError:
+        print("The source file is not found")
         return
+    print("Successfully copying a file")
